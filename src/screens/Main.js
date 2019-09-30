dd6ce26399b56806091e2f65bf0c131e413bcc9e
zaqueu lima dos santos <zaqueulima1@gmail.com>
2019-09-30 07:19:49 -0300

    First commit

---
+++ src/screens/Main.js
@@ -0,0 +1,18 @@
+import React, { PureComponent } from 'react';
+
+const Main = () => {
+  return (
+    <ScrollView style={styles.container}>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  }
+});
+
+
+export default Main
