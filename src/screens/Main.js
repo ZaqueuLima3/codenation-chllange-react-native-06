7f3db16deeb4acd40acb2085a5eeb076b362339d
zaqueu lima dos santos <zaqueulima1@gmail.com>
2019-10-02 07:57:27 -0300

    First request

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,18 +1,43 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, useEffect } from "react";
+import ApolloClient from "apollo-boost";
+import { gql } from "apollo-boost";
 
-const Main = () => {
-  return (
-    <ScrollView style={styles.container}>
-    </ScrollView>
-  )
+import { StyleSheet, ScrollView } from "react-native";
+
+const client = new ApolloClient({
+  uri: "https://graphql-pokemon.now.sh/"
+});
+
+function Main() {
+  useEffect(() => {
+    let data;
+    async function run() {
+      data = await test();
+      console.log(data);
+    }
+    run();
+  });
+  async function test() {
+    const response = await client.query({
+      query: gql`
+        {
+          pokemon(name: "Pikachu") {
+            id
+            name
+          }
+        }
+      `
+    });
+    return response;
+  }
+  return <ScrollView style={styles.container}></ScrollView>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   }
 });
 
-
-export default Main
+export default Main;
